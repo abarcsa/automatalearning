a7d90a14ec557dcfa4456679afde93f4f49252bc
Aron Barcsa-Szabo <abarcsa@gmail.com>
2020-07-09 14:38:25 +0200

    Setup for tdk

--- lpt-xtend/src/main/java/hu/bme/mit/lpt_xtend/LPT.xtend
+++ lpt-xtend/src/main/java/hu/bme/mit/lpt_xtend/LPT.xtend
@@ -9,18 +9,19 @@ import java.util.HashSet
 import java.util.Deque
 import java.util.LinkedList
 import java.util.Map.Entry
+import hu.bme.mit.lpt.LptFactory
 
 class LPT {
-	protected LPTRootNode node;
+	protected LPTRootNode root;
 	protected Set<String> inputAlphabet;
 	
 	new(LPTRootNode node){
-		this.node = node;
+		this.root = node;
 		inputAlphabet = getInputAlphabetBFS();
 	}
 	
 	def public String getOutput(List<? extends String> input){
-		var LPTRootNode node = node;
+		var LPTRootNode node = root;
 		for(String in : input){
 			node = traverse(node, in);
 		}
@@ -29,6 +30,21 @@ class LPT {
 	}
 	
 	def public Set<String> getInputAlphabet(){ return this.inputAlphabet }
+	
+	def public LPTRootNode getRoot(){
+		return root;
+	}
+	
+	def public addSequence(List<? extends String> input, String output){
+		var LPTRootNode node = root;
+		for(var i = 0; i < input.size()-1; i++){
+			node = traverse(node, input.get(i));
+		}
+		if(node.children.containsKey(input.get(input.size()-1))) throw new Exception("Sequence already exists!");
+		var newNode = LptFactory.eINSTANCE.createLPTUnloopedNode();
+		newNode.output = output;
+		node.children.put(input.get(input.size()-1), newNode);
+	}
 	
 	//Default case for Root and Unlooped nodes
 	def private dispatch traverse(LPTRootNode node, String character){
@@ -45,7 +61,7 @@ class LPT {
 		var Deque<LPTRootNode> queue = new LinkedList
 		var inputAlphabet = new HashSet
 		var visited = new HashSet //just to make sure no infinite cycles arise
-		queue.add(node); //from root
+		queue.add(root); //from root
 		while(!queue.isEmpty){
 			var curr = queue.poll
 			for(Entry<String, LPTRootNode> n : curr.children.entrySet){
@@ -59,5 +75,7 @@ class LPT {
 		}
 		return inputAlphabet;
 	}
+	
+	
 	
 }
--- lpt-xtend/xtend-gen/hu/bme/mit/lpt_xtend/LPT.java
+++ lpt-xtend/xtend-gen/hu/bme/mit/lpt_xtend/LPT.java
@@ -3,6 +3,7 @@ package hu.bme.mit.lpt_xtend;
 import hu.bme.mit.lpt.LPTLoopedNode;
 import hu.bme.mit.lpt.LPTRootNode;
 import hu.bme.mit.lpt.LPTUnloopedNode;
+import hu.bme.mit.lpt.LptFactory;
 import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashSet;
@@ -14,18 +15,18 @@ import org.eclipse.xtext.xbase.lib.Exceptions;
 
 @SuppressWarnings("all")
 public class LPT {
-  protected LPTRootNode node;
+  protected LPTRootNode root;
   
   protected Set<String> inputAlphabet;
   
   public LPT(final LPTRootNode node) {
-    this.node = node;
+    this.root = node;
     this.inputAlphabet = this.getInputAlphabetBFS();
   }
   
   public String getOutput(final List<? extends String> input) {
     try {
-      LPTRootNode node = this.node;
+      LPTRootNode node = this.root;
       for (final String in : input) {
         node = this.traverse(node, in);
       }
@@ -40,6 +41,36 @@ public class LPT {
   
   public Set<String> getInputAlphabet() {
     return this.inputAlphabet;
+  }
+  
+  public LPTRootNode getRoot() {
+    return this.root;
+  }
+  
+  public LPTRootNode addSequence(final List<? extends String> input, final String output) {
+    try {
+      LPTRootNode _xblockexpression = null;
+      {
+        LPTRootNode node = this.root;
+        for (int i = 0; (i < (input.size() - 1)); i++) {
+          node = this.traverse(node, input.get(i));
+        }
+        int _size = input.size();
+        int _minus = (_size - 1);
+        boolean _containsKey = node.getChildren().containsKey(input.get(_minus));
+        if (_containsKey) {
+          throw new Exception("Sequence already exists!");
+        }
+        LPTUnloopedNode newNode = LptFactory.eINSTANCE.createLPTUnloopedNode();
+        newNode.setOutput(output);
+        int _size_1 = input.size();
+        int _minus_1 = (_size_1 - 1);
+        _xblockexpression = node.getChildren().put(input.get(_minus_1), newNode);
+      }
+      return _xblockexpression;
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
   
   private LPTRootNode _traverse(final LPTRootNode node, final String character) {
@@ -73,7 +104,7 @@ public class LPT {
     Deque<LPTRootNode> queue = new LinkedList<LPTRootNode>();
     HashSet<String> inputAlphabet = new HashSet<String>();
     HashSet<Map.Entry<String, LPTRootNode>> visited = new HashSet<Map.Entry<String, LPTRootNode>>();
-    queue.add(this.node);
+    queue.add(this.root);
     while ((!queue.isEmpty())) {
       {
         LPTRootNode curr = queue.poll();
